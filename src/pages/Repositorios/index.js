2bd3b2b43540bd4ab2bc0c14bd35a3ec43a2b34e
Tarcisio <tarcisioemanoel10031@gmail.com>
2025-03-31 02:33:13 -0300

    first commit

---
+++ src/pages/Repositorios/index.js
@@ -0,0 +1,10 @@
+export default function Rerositorio() {
+    return (
+      <div>
+        <h1> may Rerositorio </h1>
+      </div>
+    );
+  }
+  
+  
+  
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Repositorios from './pages/Repositorios';
+import Main from './pages/Main';
+
+export default function MayRoutes(){
+    return(
+
+        <BrowserRouter>
+        
+        <Routes>
+        
+        <Route exact path='/'  Component={Main}/>
+        <Route exact path='/repositorios'  Component={Repositorios} />
+
+        </Routes>
+        </BrowserRouter>
+    )
+}
